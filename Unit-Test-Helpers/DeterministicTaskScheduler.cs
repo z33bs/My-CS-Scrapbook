f94ce226576d10e5fdb1c1108658bce96eb0d2d6
Guy Antoine <zeebsmail@gmail.com>
2020-10-21 09:43:47 +0200

    Update DeterministicTaskScheduler.cs

--- Unit-Test-Helpers/DeterministicTaskScheduler.cs
+++ Unit-Test-Helpers/DeterministicTaskScheduler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ZenMvvm.Tests
+namespace UnitTestHelpers
 {
     /// <summary>
     /// A TaskScheduler to be used for unit testing. 
